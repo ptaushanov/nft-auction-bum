5515cfbd3d6441d98e119a980c766727ca2d1ced
ptaushanov <pepi15032000@gmail.com>
2021-10-20 11:01:04 +0300

    fix: removed avatar alt property

--- src/components/avatar/Avatar.jsx
+++ src/components/avatar/Avatar.jsx
@@ -4,9 +4,9 @@ function Avatar({ size=90, url, verified=false }){
     return (
         <div className="Avatar">
             <div className={styles.avatar} style={{width: size, height: size}}>
-                <img className={styles.image} src={url} alt="profile picture" />
+                <img className={styles.image} src={url} />
                 { verified &&
-                    <img className={styles.badge} src="/images/verified.svg" alt="badge" />
+                    <img className={styles.badge} src="/images/verified.svg" />
                 }
             </div>
         </div>
