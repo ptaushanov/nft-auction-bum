2327e0d3ac57fc38b9267fdeab0c19c1867d3de3
ptaushanov <pepi15032000@gmail.com>
2021-10-29 22:53:26 +0300

    feat: better Header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,4 @@
-import Grid from "@mui/material/Grid"
-import Button from '@mui/material/Button';
-import InputBase from '@mui/material/InputBase';
-import Paper from '@mui/material/Paper';
+import {Button, InputBase, Paper, Grid, Container} from "@mui/material"
 import SearchIcon from "@mui/icons-material/Search"
 import Logo from "../logo/Logo"
 import styles from "./Header.module.scss"
@@ -9,22 +6,24 @@ import styles from "./Header.module.scss"
 function Header(){
     return (
         <div className={styles.Header}>
-            <Grid className={styles.container} container spacing={6}>
-                <Grid item style={{marginBottom: "-0.5rem"}}>
-                    <Logo/>
+            <Container>
+                <Grid className={styles.container} container>
+                    <Grid item  md={2} sx={{marginBottom: "-0.5rem"}}>
+                        <Logo/>
+                    </Grid>
+                    <Grid item md={5} sx={{display: "flex"}}>
+                        <Paper className={styles.searchPaper} sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width:400}} >
+                            <SearchIcon className = {styles.searchIcon}/>
+                            <InputBase className={styles.searchInput}  placeholder="Find items, users and activities"/>
+                        </Paper>
+                    </Grid>
+                    <Grid item md={5} sx={{display: "flex", justifyContent:"flex-end"}}>
+                        <Button className={styles.menuItem} variant={"text"}>Home</Button>
+                        <Button className={styles.menuItem} variant={"text"}>Activity</Button>
+                        <Button className={styles.menuItem} variant={"contained"}>EXPLORE</Button>
+                    </Grid>
                 </Grid>
-                <Grid item>
-                    <Paper className={styles.searchPaper} sx={{ p: '2px 4px', display: 'flex', alignItems: 'center', width:400}} >
-                        <SearchIcon className = {styles.searchIcon}/>
-                        <InputBase className={styles.searchInput}  placeholder="Find items, users and activities"/>
-                    </Paper>
-                </Grid>
-                <Grid item>
-                    <Button className={styles.menuItem} variant={"text"}>Home</Button>
-                    <Button className={styles.menuItem} variant={"text"}>Activity</Button>
-                    <Button className={styles.menuItem} variant={"contained"}>EXPLORE</Button>
-                </Grid>
-            </Grid>
+            </Container>
         </div>
     )
 }
