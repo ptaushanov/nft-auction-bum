a2e8f28ed62e33ce0beadc5e65e11bcf37869347
ptaushanov <pepi15032000@gmail.com>
2021-10-30 13:24:36 +0300

    feat: implemented full features of How component

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -7,8 +7,8 @@ export default function Copyright() {
     <div>
       <Header />
         <How 
-          title=""
-          description=""
+          title="How it works"
+          description="Irure ex et quis tempor elit voluptate nostrud. Sunt dolor est incididunt sunt quis magna non ut. Id sit in eiusmod id aute velit ad consequat. Reprehenderit irure ex do dolor nostrud culpa tempor ullamco pariatur sint. Nulla dolor et non exercitation. Aliqua excepteur culpa reprehenderit minim non enim do sint culpa anim. Elit ex qui quis eu nisi ex consectetur dolor."
           items={[
             {
               title:"Digital Currency",
@@ -23,7 +23,7 @@ export default function Copyright() {
               description:"You can get ETH and digital currency that fuels transactions on the Etherium blockchain from a digital currency exchange"
             }
           ]}
-          link=""
+          link="https://www.google.com"
         />
       <Footer />
     </div>
--- src/components/how/How.jsx
+++ src/components/how/How.jsx
@@ -1,5 +1,5 @@
 import styles from "./How.module.scss"
-import { Grid, Container } from "@mui/material"
+import { Grid, Container, Button } from "@mui/material"
 import Step from "../step/Step"
 
 function How({description, title, items=[], link}){
@@ -7,12 +7,21 @@ function How({description, title, items=[], link}){
         <div className={styles.How}>
             <Container>
                 <Grid container>
-                    <Grid item md={7}>
-                        ... WIP ...
+                    <Grid item md={7} className={styles.info}>
+                        <div className="wrapper">
+                            <h1 className={styles.title}>{title}</h1>
+                            <p className={styles.description}>{description}</p>
+                            <Button
+                                variant={"contained"}
+                                className={styles.button}
+                                href={link}
+                            >Learn More</Button>
+                        </div>
                     </Grid>
                     <Grid item md={5} className={styles.steps}>
                         {items.map((item, index) => (
                             <Step
+                                key={index}
                                 number={index+1}
                                 title={item.title}
                                 description={item.description}
