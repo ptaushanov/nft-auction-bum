7eedc1efa82c8ffda9a1ddd78c78198955176cb9
ptaushanov <pepi15032000@gmail.com>
2021-10-31 13:39:47 +0200

    feat: implemented TopCollectors component

--- src/components/example/Example.jsx
+++ src/components/example/Example.jsx
@@ -1,28 +1,73 @@
 import Header from "../header/Header";
 import Footer from "../footer/Footer"
-import CollectorColumn from "../collectors/CollectorColumn";
+import TopCollectors from "../collectors/TopCollectors";
 
 export default function Copyright() {
   return (
     <div>
       <Header />
-      <CollectorColumn 
-        items={
+      <TopCollectors 
+        collectors={
         [{
-          id: 1,
           name: "Peter",
           nftsCount: 12312,
           avatar: "/images/avatar.png",
           verified: true,
         },
         {
-          id: 2,
           name: "John",
           nftsCount: 1111,
           avatar: "/images/avatar.png",
         },
         {
-          id: 3,
+          name: "Steven",
+          nftsCount: 432,
+          avatar: "/images/avatar.png",
+        },
+        {
+          name: "Peter",
+          nftsCount: 12312,
+          avatar: "/images/avatar.png",
+          verified: true,
+        },
+        {
+          name: "John",
+          nftsCount: 1111,
+          avatar: "/images/avatar.png",
+        },
+        {
+          name: "Steven",
+          nftsCount: 432,
+          avatar: "/images/avatar.png",
+        },
+        {
+          name: "Peter",
+          nftsCount: 12312,
+          avatar: "/images/avatar.png",
+          verified: true,
+        },
+        {
+          name: "John",
+          nftsCount: 1111,
+          avatar: "/images/avatar.png",
+        },
+        {
+          name: "Steven",
+          nftsCount: 432,
+          avatar: "/images/avatar.png",
+        },
+        {
+          name: "Peter",
+          nftsCount: 12312,
+          avatar: "/images/avatar.png",
+          verified: true,
+        },
+        {
+          name: "John",
+          nftsCount: 1111,
+          avatar: "/images/avatar.png",
+        },
+        {
           name: "Steven",
           nftsCount: 432,
           avatar: "/images/avatar.png",
