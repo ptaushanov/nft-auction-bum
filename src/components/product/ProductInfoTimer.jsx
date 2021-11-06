dd0279d3e862e3704f4ad83a170dd0540211a78e
ptaushanov <pepi15032000@gmail.com>
2021-11-06 21:22:31 +0200

    fix: fixed countdown not hiding

--- src/components/product/ProductInfoTimer.jsx
+++ src/components/product/ProductInfoTimer.jsx
@@ -1,27 +1,24 @@
 import styles from "./ProductInfoTimer.module.scss"
 import Countdown from 'react-countdown';
-import { Fragment } from "react";
 
-export default function ProductInfoTimer({timeEnd, onTimeEnd}){
+export default function ProductInfoTimer({timeEnd=null, onTimeEnd}){
     return (
         <div 
             className={`${styles["product-info-timer"]} ${timeEnd && styles.active}`}>
-            { timeEnd && (
-                <Fragment>
-                    <div className={styles.header}>
-                        <p className={styles.title}>Ends in</p>
-                    </div>
-                    <div className={styles.timer}>
-                        <Countdown 
-                            onComplete={onTimeEnd}
-                            date={Date.now() + timeEnd}
-                            renderer = {({ hours, minutes, seconds }) => (
-                                <span>{hours}:{minutes}:{seconds}</span>
-                            )}
-                          />
-                    </div>
-                </Fragment>
-            )}
+            <div className={styles.header}>
+                <p className={styles.title}>Ends in</p>
+            </div>
+            <div className={styles.timer}>
+                { timeEnd  && (
+                <Countdown 
+                    onComplete={onTimeEnd}
+                    date={Date.now() + timeEnd}
+                    renderer = {({ hours, minutes, seconds }) => (
+                        <span>{hours}:{minutes}:{seconds}</span>
+                    )}
+                    />
+                )}
+            </div>
         </div>
     )
 }
