822959056b88b92b416bc85180f0cf33c4a234a7
ptaushanov <pepi15032000@gmail.com>
2021-11-06 16:03:59 +0200

    feat: implemented ProductInfoTimer component

---
+++ src/components/product/ProductInfoTimer.jsx
@@ -0,0 +1,26 @@
+import styles from "./ProductInfoTimer.module.scss"
+import Countdown from 'react-countdown';
+import { Fragment } from "react";
+
+export default function ProductInfoTimer({timeEnd=null, onTimeEnd}){
+    return (
+        <div className={styles["product-info-timer"]}>
+            { timeEnd && (
+                <Fragment>
+                    <div className={styles.header}>
+                        <p className={styles.title}>Ends in</p>
+                    </div>
+                    <div className={styles.timer}>
+                        <Countdown 
+                            onComplete={onTimeEnd}
+                            date={Date.now() + timeEnd}
+                            renderer = {({ hours, minutes, seconds }) => (
+                                <span>{hours}:{minutes}:{seconds}</span>
+                            )}
+                          />
+                    </div>
+                </Fragment>
+            )}
+        </div>
+    )
+}
