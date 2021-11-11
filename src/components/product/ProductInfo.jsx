aa51bf1b4d65ea856f2c48e3d7e6a199babf0e9f
ptaushanov <pepi15032000@gmail.com>
2021-11-11 18:50:18 +0200

    fix: class name and props

--- src/components/product/ProductInfo.jsx
+++ src/components/product/ProductInfo.jsx
@@ -7,13 +7,11 @@ import ProductInfoCreator from "./ProductInfoCreator"
 import ProductInfoTimer from "./ProductInfoTimer"
 import { Stack, Grid } from "@mui/material"
 
-export default function ProductInfo(props){
-    const {title, creator, price, currency, likes, onTimeEnd, timeEnd, isLive} = props;
-
+export default function ProductInfo({title, creator, price, currency, likes, onTimeEnd, timeEnd, isLive}){
     return (
         <div className={styles["product-info"]}>
             <ProductInfoTitle text={title}/>
-            <Stack className={styles.status} direction={"row"}>
+            <Stack className={styles.stats} direction={"row"}>
                 <ProductInfoPrice amount={price} currency={currency}/>
                 {isLive && (<ProductInfoStatus/>)}
                 <ProductInfoLikes amount={likes}/>
