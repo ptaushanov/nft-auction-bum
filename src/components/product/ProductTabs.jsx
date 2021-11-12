e82f0cc281fe21ec44e5cd63601444f5985dd492
ptaushanov <pepi15032000@gmail.com>
2021-11-13 00:34:50 +0200

    feat: implemented full features for ProductTabs

--- src/components/product/ProductTabs.jsx
+++ src/components/product/ProductTabs.jsx
@@ -7,7 +7,7 @@ import formatDistance from 'date-fns/formatDistance'
 
 import { useState } from 'react'
 
-export default function ProductTabs({ text, bids }){
+export default function ProductTabs({ text, bids=[] }){
     const [value, setValue] = useState("1")
     const handleTabChange = (event, newValue) => setValue(newValue); 
 
@@ -18,21 +18,23 @@ export default function ProductTabs({ text, bids }){
                     <Tab label={"Details"} value="1" className={styles["tab-details"]}/>
                     <Tab label={"Bids"}  value="2" className={styles["tab-bids"]}/>
                 </TabList>
-                <TabPanel value="1">
+                <TabPanel value="1">{ text }</TabPanel>
+                <TabPanel value="2">
                     <Table area-label={"product details table"}>
                         <TableBody>
-                            {/* There should be .map() */}
-                            <TableRow className={`table-row-${0}`}>
-                                <TableCell>
-                                    <User />
-                                </TableCell>
-                                <TableCell>price and currency</TableCell>
-                                <TableCell>when</TableCell>
-                            </TableRow>
+                            { bids.map((bid, i) => (
+                                <TableRow className={`table-row-${i}`} key={i}>
+                                    <TableCell>
+                                        <User {...bid.user} />
+                                    </TableCell>
+                                    <TableCell>{bid.amount}</TableCell>
+                                    <TableCell>{formatDistance(parseISO(bid.date), Date.now())}</TableCell>
+                                </TableRow>
+                                ))
+                            }
                         </TableBody>
                     </Table>
                 </TabPanel>
-                <TabPanel value="2"></TabPanel>
             </TabContext>
         </div>
     )
