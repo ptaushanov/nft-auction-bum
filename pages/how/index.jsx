4e5f6aa2bf1e5cec9f9416ee4fa9187fdc6af83e
ptaushanov <pepi15032000@gmail.com>
2021-11-14 20:51:09 +0200

    fix: fixed styles for How page

--- pages/how/index.jsx
+++ pages/how/index.jsx
@@ -9,9 +9,14 @@ export default function How() {
       <Header />
       <Hero text={"How it works"} />
       <Description
-        text={
-          "Occaecat incididunt cillum irure ea Lorem officia eu adipisicing deserunt tempor laboris ea irure qui. Magna qui pariatur consequat voluptate. Nostrud non incididunt excepteur aliquip irure do laborum tempor. Nostrud mollit ex reprehenderit velit. Enim dolore eiusmod exercitation ut est in. Id aliquip cillum non duis nostrud sunt qui magna."
-        }
+        text={`Ullamco deserunt consequat adipisicing ipsum qui ea amet et aliqua anim deserunt. Quis tempor minim ea tempor cillum. Proident magna voluptate ad officia cillum. Sint qui laboris quis ea ullamco exercitation tempor.
+Exercitation qui sit veniam irure laborum ipsum duis ut do exercitation eiusmod. Fugiat commodo consequat pariatur dolor quis excepteur. Eu amet laborum commodo eiusmod aliqua ullamco. Nulla laboris nostrud mollit eiusmod sit irure do nulla ut qui culpa occaecat dolore ipsum.
+Ut deserunt duis ut mollit nostrud nisi aliquip incididunt magna est nisi ullamco Lorem. Cupidatat eu nostrud pariatur fugiat ex veniam et excepteur. Duis ut et sunt qui.
+Excepteur ipsum consequat enim laborum ad ipsum nostrud aliquip deserunt fugiat. Ex tempor aliquip fugiat duis irure consectetur. Nisi non elit et in aliquip.
+Nostrud est aliquip eiusmod occaecat laborum deserunt labore nisi sint. Deserunt sunt occaecat esse tempor consectetur cupidatat eiusmod duis. Amet dolore voluptate ullamco minim aliqua irure sint commodo culpa adipisicing consectetur duis amet. Exercitation duis deserunt ipsum ex eu do laboris elit non occaecat laborum amet. Incididunt culpa aliqua commodo aliquip aute eu consectetur enim sint consequat pariatur cupidatat nisi elit. Cupidatat enim esse proident aliquip.
+Commodo nostrud dolore Lorem dolor nulla do. Deserunt veniam ea minim sunt nulla deserunt eu officia commodo velit esse tempor. Eu proident exercitation consequat nostrud amet. Ex commodo aliqua enim eiusmod aliquip elit ex nisi dolor aliqua sunt ut voluptate laborum. Nulla commodo veniam anim mollit magna excepteur incididunt ex incididunt labore nisi aliqua. Elit tempor enim deserunt ad aliquip dolore quis laboris. Non elit sunt ad mollit do exercitation nostrud do duis reprehenderit incididunt minim.
+Ea qui ullamco labore velit velit anim culpa. Lorem aute anim sunt labore pariatur. Anim ullamco ut excepteur fugiat amet labore sit ipsum nostrud exercitation nulla.
+`}
         image={"images/nft.jpg"}
       />
       <Footer />
--- src/components/description/Description.jsx
+++ src/components/description/Description.jsx
@@ -1,13 +1,15 @@
 import styles from "./Description.module.scss";
-import Typography from "@mui/material/Typography";
+import { Typography, Container } from "@mui/material";
 
 export default function Description({ text, image }) {
   return (
     <div className={styles.description}>
-      <Typography variant={"body1"} className={styles.text}>
-        {text}
-      </Typography>
-      <img src={image} className={styles.image} />
+      <Container className={styles.container}>
+        <Typography variant={"body1"} className={styles.text}>
+          {text}
+        </Typography>
+        <img src={image} className={styles.image} />
+      </Container>
     </div>
   );
 }
