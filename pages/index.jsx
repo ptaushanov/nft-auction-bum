23b8066414b9d31ad88891e3625140dd6c71d535
ptaushanov <pepi15032000@gmail.com>
2021-11-19 18:04:25 +0200

    feat: implemented Trending component API calls

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,84 +1,105 @@
 import Header from "../src/components/header/Header";
-import Featured from "../src/components/featured/Featured"
-import Trending from "../src/components/trending/Trending"
-import TopCollectors from "../src/components/collectors/TopCollectors"
-import How from "../src/components/how/How"
-import Auctions from "../src/components/auctions/Auctions"
-import Footer from "../src/components/footer/Footer"
+import Featured from "../src/components/featured/Featured";
+import Trending from "../src/components/trending/Trending";
+import TopCollectors from "../src/components/collectors/TopCollectors";
+import How from "../src/components/how/How";
+import Auctions from "../src/components/auctions/Auctions";
+import Footer from "../src/components/footer/Footer";
 
-import dataFeatured from "../data/featured.json"
-import dataTrending from "../data/trending.json"
-import dataUsers from "../data/users.json"
-import dataNfts from "../data/nfts.json"
+import dataFeatured from "../data/featured.json";
+import dataUsers from "../data/users.json";
+import dataNfts from "../data/nfts.json";
 
 import { useState, useEffect } from "react";
 
-
 export default function Index() {
+  const [featuredCards, setFeaturedCards] = useState([]);
+  const [trendingItems, setTrendingItems] = useState([]);
+  const [trendingFilters, setTrendingFilters] = useState(null);
+  const [topCollectors, setTopCollectors] = useState([]);
+  const [nfts, setNfts] = useState([]);
 
-  const [featuredCards, setFeaturedCards] = useState([])
-  const [trendingCards, setTrendingCards] = useState([])
-  const [topCollectors, setTopCollectors] = useState([])
-  const [nfts, setNfts] = useState([])
+  useEffect(async () => {
+    // Featred
+    const processedFeatured = dataFeatured.map(card => {
+      return { image: card.source.url };
+    });
+    processedFeatured[0] = { ...processedFeatured[0], cols: 3, rows: 2 };
+    setFeaturedCards(processedFeatured);
 
-  useEffect(()=>{
-    const processedFeatured = dataFeatured.map(card => { return {image: card.source.url}})
-    processedFeatured[0] = {...processedFeatured[0], cols:3, rows: 2}
-    setFeaturedCards(processedFeatured)
-    
-    setTrendingCards(dataTrending.map(card => {
-      return {
-        name: card.name,
-        likes:card.likes,
-        mediaUrl: card.source.url,
-        user:{
-          avatarUrl: card.owner.avatar.url,
-          verified: card.owner.verified
-        },
-        price: card.price,
-        currency: card.currency
-      }
-    }));
+    // Trending
+    try {
+      const dataTrending = await fetch(process.env.apiUrl + "/trending").then(
+        res => res.json()
+      );
 
-    setNfts(dataNfts.map(nft => {
-      return {
-        name: nft.name,
-        likes:nft.likes,
-        mediaUrl: nft.source.url,
-        user:{
-          avatarUrl: nft.owner.avatar.url,
-        },
-        price: nft.price,
-        currency: nft.currency,
-        timeLeft: Math.abs(Date.parse(nft.auction_end) - Date.now())
-      }
-    }));
+      // Trending Nfts
+      setTrendingItems(
+        dataTrending.nfts.map(card => {
+          return {
+            name: card.name,
+            likes: card.likes,
+            mediaUrl: card.source.url,
+            user: {
+              avatarUrl: card.owner.avatar.url,
+              verified: card.owner.verified,
+            },
+            price: card.price,
+            currency: card.currency,
+          };
+        })
+      );
+
+      // Trending Filters
+      setTrendingFilters(dataTrending.filters);
+    } catch (error) {
+      // Fetch error or TypeError
+      console.error(error.message);
+    }
+
+    // Nfts for Live Auction
+    setNfts(
+      dataNfts.map(nft => {
+        return {
+          name: nft.name,
+          likes: nft.likes,
+          mediaUrl: nft.source.url,
+          user: {
+            avatarUrl: nft.owner.avatar.url,
+          },
+          price: nft.price,
+          currency: nft.currency,
+          timeLeft: Math.abs(Date.parse(nft.auction_end) - Date.now()),
+        };
+      })
+    );
 
     setTopCollectors(
-      dataUsers.map(user => {
-        return {
-          name: user.username,
-          nftsCount: user.nfts.length,
-          avatar: user.avatar.url,
-          verified: user.verified
-        }
-      }).sort((f,s)=> f.nftsCount < s.nftsCount).slice(0, 12)
-    )
-  },[])
-
+      dataUsers
+        .map(user => {
+          return {
+            name: user.username,
+            nftsCount: user.nfts.length,
+            avatar: user.avatar.url,
+            verified: user.verified,
+          };
+        })
+        .sort((f, s) => f.nftsCount < s.nftsCount)
+        .slice(0, 12)
+    );
+  }, []);
 
   return (
     <div>
-      <Header/>
+      <Header />
       <Featured items={featuredCards} />
-      <Trending cards={trendingCards}/>
-      <TopCollectors collectors={topCollectors}/>
-      <How 
+      <Trending cards={trendingItems} filters={trendingFilters} />
+      <TopCollectors collectors={topCollectors} />
+      <How
         title="How it works"
         description="Discover, collect, and sell extraordinary NFTs on the world's first and largest NFT marketplace. There are  three things you'll need in place to open your account and start buying or selling NFTs on BUM."
-
-        items={
-          [{
+        items={[
+          {
             title: "Digital Currency",
             description:
               "You can get ETH, the digital currency that fuels transactions on the Ethereum blockchain, from a digital currency exchange",
@@ -92,11 +113,12 @@ export default function Index() {
             title: "BUM.",
             description:
               "Let's connect your wallet to BUM, edit your profile, and begin interacting in the space.",
-          }]
-        }
-        link="https://app.boom.dev"/>
-      <Auctions cards={nfts}/>
-      <Footer/>
+          },
+        ]}
+        link="https://app.boom.dev"
+      />
+      <Auctions cards={nfts} />
+      <Footer />
     </div>
-  )
+  );
 }
--- src/components/trending/Trending.jsx
+++ src/components/trending/Trending.jsx
@@ -1,33 +1,53 @@
-import styles from "./Trending.module.scss"
-import { FormControl, Grid, Select, MenuItem, Container, InputLabel } from "@mui/material"
-import Card from "../card/Card"
+import styles from "./Trending.module.scss";
+import {
+  FormControl,
+  Grid,
+  Select,
+  MenuItem,
+  Container,
+  InputLabel,
+} from "@mui/material";
+import Card from "../card/Card";
+import { useState } from "react";
 
-export default function Trending({cards=[]}){
-    return (
-        <div className={styles.Trending}>
-            <Container>
-                <Grid container>
-                    <Grid item xs={10}>
-                        <h1 className={styles.title}>Trending</h1>
-                    </Grid>
-                    <Grid item xs={2} sx={{display: "flex"}}>
-                        <FormControl fullWidth className={styles.select} size={"small"}>
-                            <InputLabel id="select-label">Sort By</InputLabel>
-                            <Select labelId="select-label">
-                                <MenuItem>This week</MenuItem>
-                            </Select>
-                        </FormControl>
-                    </Grid>
-                </Grid>
-                <Grid container spacing={2}>
-                    { cards.slice(0, 4).map((card, index) => (
-                            <Grid item md={3} key={index}>
-                                <Card {...card} />
-                            </Grid>
-                        ))
-                    }
-                </Grid>
-            </Container>
-        </div>
-    )
+export default function Trending({ cards = [], filters }) {
+  const [sortBy, setSortBy] = useState("");
+
+  return (
+    <div className={styles.Trending}>
+      <Container>
+        <Grid container>
+          <Grid item xs={10}>
+            <h1 className={styles.title}>Trending</h1>
+          </Grid>
+          <Grid item xs={2} sx={{ display: "flex" }}>
+            <FormControl fullWidth className={styles.select} size={"small"}>
+              <InputLabel id="select-label">Sort By</InputLabel>
+              <Select
+                labelId="select-label"
+                value={sortBy}
+                onChange={e => setSortBy(e.target.value)}
+              >
+                {filters &&
+                  filters.sort.map(filter => {
+                    return (
+                      <MenuItem key={filter.label} value={filter.value}>
+                        {filter.label}
+                      </MenuItem>
+                    );
+                  })}
+              </Select>
+            </FormControl>
+          </Grid>
+        </Grid>
+        <Grid container spacing={2}>
+          {cards.slice(0, 4).map((card, index) => (
+            <Grid item md={3} key={index}>
+              <Card {...card} />
+            </Grid>
+          ))}
+        </Grid>
+      </Container>
+    </div>
+  );
 }
